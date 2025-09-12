e5662aa0adec73d75c01421978b5a892c169a6dc
Md. Asif Shahriar Tauhid <asiftauhid4665@gmail.com>
2025-09-13 01:43:23 +0600

    Edited song cards

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function getSongs() {
   for (let i = 0; i < as.length; i++) {
     const element = as[i];
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href);
+      songs.push(element.href.split("/songs/")[1]);
     }
   }
   return songs;
@@ -19,6 +19,37 @@ async function getSongs() {
 async function main() {
   let songs = await getSongs();
   console.log(songs);
+
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
+
+  for (const song of songs) {
+    songUL.innerHTML =
+      songUL.innerHTML +
+      `<li>
+                <img class="invert" src="img/music.svg" alt="">
+                <div class="info">
+                  <div> ${song.replaceAll("%20", " ")} </div>
+                  <div>Unknown</div>
+                </div>
+                <div class="playnow">
+                  <span>Play Now</span>
+                  <img class="invert" src="img/play.svg" alt="" >
+                </div>
+              </li>`;
+  }
+
+  // playing a song.
+  var audio = new Audio(songs[0]);
+  //audio.play();
+
+  audio.addEventListener("loadeddata", () => {
+    let duration = audio.duration;
+    console.log(duration);
+  });
 }
 
 main();
+
+//Next we will start from 2:10:43
