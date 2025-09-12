9ece9fee95a4685a49aaa75b041aa94c642bdef7
Md. Asif Shahriar Tauhid <asiftauhid4665@gmail.com>
2025-09-13 00:27:13 +0600

    tried to add songs

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Let's try javascript.");
+
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let response = await a.text();
+  //console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  for (let i = 0; i < as.length; i++) {
+    const element = as[i];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href);
+    }
+  }
+  return songs;
+}
+async function main() {
+  let songs = await getSongs();
+  console.log(songs);
+}
+
+main();
