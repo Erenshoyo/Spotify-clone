52d154ce80afda8c416ead360a1663a0ca0646c5
Md. Asif Shahriar Tauhid <asiftauhid4665@gmail.com>
2025-09-13 13:00:01 +0600

    Music controls (Play, pause, static seeking) added

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 console.log("Let's try javascript.");
 
+let currentSong = new Audio();
+
+function secondsToMinutesSeconds(seconds) {
+  if (isNaN(seconds) || seconds < 0) {
+    return "00:00";
+  }
+
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:5500/songs/");
   let response = await a.text();
@@ -16,9 +32,21 @@ async function getSongs() {
   }
   return songs;
 }
+
+const playMusic = (track, pause = false) => {
+  //let audio = new Audio("/songs/" + track);
+  currentSong.src = "/songs/" + track;
+  if (!pause) {
+    currentSong.play();
+    play.src = "img/pause.svg";
+  }
+  document.querySelector(".songinfo").innerHTML = decodeURI(track);
+  document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+};
+
 async function main() {
   let songs = await getSongs();
-  console.log(songs);
+  playMusic(songs[0], true);
 
   let songUL = document
     .querySelector(".songList")
@@ -28,28 +56,53 @@ async function main() {
     songUL.innerHTML =
       songUL.innerHTML +
       `<li>
-                <img class="invert" src="img/music.svg" alt="">
-                <div class="info">
-                  <div> ${song.replaceAll("%20", " ")} </div>
-                  <div>Unknown</div>
-                </div>
-                <div class="playnow">
-                  <span>Play Now</span>
-                  <img class="invert" src="img/play.svg" alt="" >
-                </div>
-              </li>`;
+    <img class="invert" src="img/music.svg" alt="">
+    <div class="info">
+    <div> ${song.replaceAll("%20", " ")} </div>
+    <div>Unknown</div>
+    </div>
+    <div class="playnow">
+    <span>Play Now</span>
+    <img class="invert" src="img/play.svg" alt="" >
+    </div>
+    </li>`;
   }
 
-  // playing a song.
-  var audio = new Audio(songs[0]);
-  //audio.play();
+  Array.from(
+    document.querySelector(".songList").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (element) => {
+      console.log(e.querySelector(".info").firstElementChild.innerHTML);
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+    });
+  });
 
-  audio.addEventListener("loadeddata", () => {
-    let duration = audio.duration;
-    console.log(duration);
+  play.addEventListener("click", () => {
+    if (currentSong.paused) {
+      currentSong.play();
+      play.src = "img/pause.svg";
+    } else {
+      currentSong.pause();
+      play.src = "img/play.svg";
+    }
+  });
+
+  currentSong.addEventListener("timeupdate", () => {
+    console.log(currentSong.currentTime, currentSong.duration);
+    document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(
+      currentSong.currentTime
+    )}/${secondsToMinutesSeconds(currentSong.duration)}`;
+    document.querySelector(".circle").style.left =
+      (currentSong.currentTime / currentSong.duration) * 100 + "%";
+  });
+
+  document.querySelector(".seekbar").addEventListener("click", (e) => {
+    let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+    document.querySelector(".circle").style.left = percent + "%";
+    currentSong.currentTime = (currentSong.duration * percent) / 100;
   });
 }
 
 main();
 
-//Next we will start from 2:10:43
+//Next we will start from 2:58:15
